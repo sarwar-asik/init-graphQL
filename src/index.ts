0b29c2d963b8c68c531d92dffd77311d8619ed40
sarwar-asik <asikpc2022@gmail.com>
2023-12-03 16:59:29 +0600

    run server

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+const courseName:string = "my graphql Course"
 
-import { typeDefs } from "../gql/schema/index.js";
-import { resolvers } from "../gql/resolver/index.js";
-
-const server = new ApolloServer({
-  typeDefs,   //! for schema
-  resolvers,  // ! for resolver or model
-});
-
-
-
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
-
-console.log(`🚀  Server ready at: ${url}`);
+console.log(courseName)
