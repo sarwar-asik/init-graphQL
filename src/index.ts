d809ebe107796e82373b271de1c31441b8194f95
sarwar-asik <asikpc2022@gmail.com>
2023-12-03 17:43:08 +0600

    apollo ran

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,48 @@
-const courseName:string = "my graphql Courses"
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
-console.log(courseName)
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const main = async () => {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4001 },
+  });
+
+  console.log(`🚀  Server ready at: ${url}`);
+};
+
+main();
